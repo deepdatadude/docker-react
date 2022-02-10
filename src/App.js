21aee82ba99ea6d2c73789d12f73be1e385c52b0
Sandeep Mohan <sjmohan@sandeeps-air.lan>
2022-02-10 16:39:41 -0500

    initial commit
    Committer: Sandeep Mohan <deepdatadude@gmail.com>

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello To React
         </a>
       </header>
     </div>
